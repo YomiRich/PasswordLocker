f3070cebe67ed5670d86bff9255851be5fa7b32a
Yomi Rich <ymueni@gmail.com>
2019-09-02 10:55:39 +0300

    Add subclass TestUser

--- test_user.py
+++ test_user.py
@@ -1,5 +1,13 @@
 import unittest
 from user import User #importing the User class
 
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours
+
+    Args:
+        unnitest.TestCase:TestCase class that helps in creating test cases
+    '''
+
         
         
