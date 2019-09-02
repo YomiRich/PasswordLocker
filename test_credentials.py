464ce05ad6617a0b49d3b1121b7021d6c3228c27
Yomi Rich <ymueni@gmail.com>
2019-09-02 13:19:26 +0300

    Add credentials object

--- test_credentials.py
+++ test_credentials.py
@@ -1,3 +1,25 @@
 import unittest
 from credentials import Credential
 
+class TestCredential(unittest.TestCase):
+    '''
+    Test class that defines test cases for the credential class behaviours
+
+    Arg:
+        unittest.TestCase:Test case class that helps in creating test cases
+    '''
+    
+    def setUp(self):
+        '''
+        setUp method to run before each test case
+        '''
+
+        #create a credential obj
+        self.new_credential=Credential("Cool","Yes","Win","Winwin")
+
+    def tearDown(self):
+        '''
+        tearDown method that cleans up after each test case is run
+        '''
+
+        Credential.credential_list=[]
